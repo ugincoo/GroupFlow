b75ea5d957125e1f1774958bbdffd728958d260f
504 <sksk1277@naver.com>
2023-05-04 16:35:17 +0900

    5/4 4:34

--- src/main/java/groupflow/controller/EmployeeController.java
+++ src/main/java/groupflow/controller/EmployeeController.java
@@ -1,7 +1,12 @@
 package groupflow.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
+@RequestMapping("employee")
 public class EmployeeController {
+
 }
