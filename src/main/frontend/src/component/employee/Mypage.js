e0c893fadb73b2bb613957b93d64b1abb96f8587
김은영 <us7677@naver.com>
2023-05-08 19:57:09 +0900

    5/8 출근,퇴근 리액트 구현

---
+++ src/main/frontend/src/component/employee/Mypage.js
@@ -0,0 +1,41 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import { Box, Typography, TextField, Button, Grid } from '@mui/material';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { blue } from '@mui/material/colors';
+//---------------------------테이블-------------------------------------
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+//---------------------------컨테이너-------------------------------------
+import Container from '@mui/material/Container';
+
+export default function Mypage(props) {
+
+    const[mypage, setmypage]= useState();
+
+    const gowork=()=>{
+        axios.post("http://localhost:8080/employee/gowork").then((r)=>{
+            console.log(r.data)
+        })
+
+    }
+    const outwork=()=>{
+        axios.put("http://localhost:8080/employee/outwork").then((r)=>{
+            console.log(r.data)
+        })
+
+    }
+
+    return(<>
+        <Container>
+            <button type="button" onClick={gowork}  > 출근 </button>
+            <button type="button" onClick={outwork}  > 퇴근 </button>
+
+        </Container>
+    </>)
+}
