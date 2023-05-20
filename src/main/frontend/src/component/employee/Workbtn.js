1e44414e168627f89745df15d34a3567d2e5987c
김은영 <us7677@naver.com>
2023-05-20 20:02:53 +0900

    5/20 출근상태완료

--- src/main/frontend/src/component/employee/Workbtn.js
+++ src/main/frontend/src/component/employee/Workbtn.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 import AttendanceStatus from './AttendanceStatus';
 
 export default function Workbtn(props) {
- let ws=useRef(null);    //장민정 서버소켓
- const [connectEmployee,setConnectEmployee]=useState(); //출근한 사람들 (소켓에 접속한 전직원)
+
     const gowork=()=>{
 
     axios.post("/employee/gowork").then((r)=>{
@@ -12,35 +11,18 @@ export default function Workbtn(props) {
             if(r.data==true){
                 alert("출근등록 되었습니다.")
                 props.getAttendance();
-                 ws.current=new WebSocket("ws://localhost:8080/commute/"+props.eno);
-                 props.gokDisabledHandler()
-
-                 ws.current.onopen=()=>{ //서버접속과 동시에 메세지센드 함수발동
-                  console.log("서버접속")
-                 // ws.current.send(JSON.stringify({eno:props.eno}))
-                  }
-
-                 ws.current.onclose=(e)=>{ console.log("나감") }
-
-
-                 ws.current.onmessage=(e)=>{
-                 console.log("메세지")
-                 const data = JSON.parse(e.data);
-                 setConnectEmployee(data)
-                  }
+                props.gokDisabledHandler()
 
             }
          })
        }
-console.log(connectEmployee)
 
 const outwork=()=>{
         axios.put("/employee/outwork").then((r)=>{
             console.log(r.data);
             if(r.data==true){alert("퇴근등록 되었습니다.");
                props.getAttendance();
-                props.gokDisabledHandler()
-                ws.current.close() }
+                props.gokDisabledHandler()}
                 })
              }
 
@@ -58,6 +40,6 @@ const outwork=()=>{
                 disabled={props.gokDisabled ? false : true}
                 onClick={outwork}  > 퇴근 </button>
 
-            <AttendanceStatus  connectEmployee={connectEmployee}/>
+
     </>)
  }
