33072f6f5360f768ab47208714a7f13634447a25
김은영 <us7677@naver.com>
2023-05-23 01:34:52 +0900

    5/23 오후1시34분 공지사항 출력

--- src/main/frontend/src/component/employee/Notice.js
+++ src/main/frontend/src/component/employee/Notice.js
@@ -5,12 +5,17 @@ import Container from '@mui/material/Container';
 
 export default function Notice(props) {
 
+const[write,setWrite]=useState([]);
 const onWriteHandler=()=>{
         let content=document.querySelector('.content').value
         axios.post("/notice/noticepost",{content:content}).then(r=>{
+        setWrite(r.data);
         console.log(r.data);
+        document.querySelector('.content').value='';
         })
     }
+    useEffect(()=>{},[])
+
 
     return(<>
         <Container>
---
+++ src/main/frontend/src/component/employee/NoticePrint.js
@@ -0,0 +1,28 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+//---------------------------컨테이너-------------------------------------
+import Container from '@mui/material/Container';
+
+export default function NoticePrint(props) {
+
+const[nnotice,setNotice]=useState([]);
+const getnotice=()=>{
+     axios.get("/notice/noticeget").then((r) => {
+        setNotice(r.data);
+        console.log(r.data);
+     })
+     }//함수만 정의해놓은것
+
+      useEffect(()=>{getnotice();},[])//페이지가 한번 열릴떄마다 실행
+
+
+    return(<>
+            {
+                nnotice.map(r=>{
+                    return(<span style={{color: "yellow", backgroundColor: "black"}}>{r.content}</span>)
+
+                })
+
+            }
+    </>)
+}
