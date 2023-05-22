9bdfcd4263ceec270aadb7795208ac12a2e5e3b7
김은영 <us7677@naver.com>
2023-05-22 15:00:57 +0900

    5/22 오후3시 오유진 취합

--- src/main/frontend/src/component/Header.js
+++ src/main/frontend/src/component/Header.js
@@ -50,7 +50,7 @@ export default function Header(props) {
             return;
           }
 
-        };
+        }; 
         const list = (anchor) => (
             <Box
               role="presentation"
