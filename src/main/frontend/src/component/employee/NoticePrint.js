69f827310145853c4e56ec02b1804e395c8a4e6c
김은영 <us7677@naver.com>
2023-05-23 18:41:11 +0900

    5/23 오후6시40분

--- src/main/frontend/src/component/employee/NoticePrint.js
+++ src/main/frontend/src/component/employee/NoticePrint.js
@@ -14,12 +14,17 @@ const getnotice=()=>{
      }//함수만 정의해놓은것
 
       useEffect(()=>{getnotice();},[])//페이지가 한번 열릴떄마다 실행
-
+      
+let [ login , setLogin ] = useState( JSON.parse(localStorage.getItem("login_token")) )
 
     return(<>
             {
                 nnotice.map(r=>{
-                    return(<span style={{color: "black",fontWeight: "bold",backgroundColor: "aliceblue" ,marginRight:"20px"}}>{r.content}</span>)
+                    return( login!==null ?
+
+                    <span style={{color: "black",fontWeight: "bold",backgroundColor: "aliceblue" ,marginRight:"20px"}}>{r.content}</span>
+                   :""
+                   )
 
                 })
 
