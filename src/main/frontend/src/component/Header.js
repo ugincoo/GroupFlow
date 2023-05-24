3ce9da654fb4cacc6a83621e604672eaae912202
김은영 <us7677@naver.com>
2023-05-24 18:22:07 +0900

    5/24 오후6시22분

--- src/main/frontend/src/component/Header.js
+++ src/main/frontend/src/component/Header.js
@@ -129,7 +129,7 @@ export default function Header(props) {
                               </ListItem>
                             ))}
                           </List> :'')
-                     }
+                     } 
 
 
                     </Box>
