9550bfb0adb274b30cc1c44e3bfbb21542d2ed68
김은영 <us7677@naver.com>
2023-05-24 17:53:42 +0900

    5/24 오후5시53분 오유진 취합

---
+++ src/main/java/groupflow/controller/WeatherController.java
@@ -0,0 +1,24 @@
+package groupflow.controller;
+
+import groupflow.service.WeatherService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/weather")
+@Slf4j
+public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+    @GetMapping("/")
+    public String getWeather(){
+
+       return weatherService.getWeather();
+    }
+}
